5df764eae2c123491ff728a92d82c753d7789c2f
useer1337 <koval.alexander00@gmail.com>
2020-07-07 18:58:29 +0500

    Начало проекта

---
+++ src/main/java/com/example/Archivist/ArchivistApplication.java
@@ -0,0 +1,13 @@
+package com.example.Archivist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Archivist/ArchivistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Archivist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
